8acd29f8c9e891b1be381ffa44d5c6034d6b63bd
Villiam Strandh <villiamstrandh@Villiams-MacBook-Pro.local>
2025-01-25 21:11:46 +0100

    throwonerror on mutate supabase

--- packages/supabase/src/mutations.ts
+++ packages/supabase/src/mutations.ts
@@ -6,19 +6,20 @@ import {
   SupabaseClient,
   UpdateParticipant,
   UpdateSubmission,
-} from './types'
-import { toCamelCase, toSnakeCase } from './utils/format-helpers'
+} from "./types";
+import { toCamelCase, toSnakeCase } from "./utils/format-helpers";
 
 export async function createParticipant(
   supabase: SupabaseClient,
   dto: InsertParticipant,
 ) {
-  const { data, ...resp } = await supabase
-    .from('participants')
+  const { data } = await supabase
+    .from("participants")
     .insert(toSnakeCase(dto))
     .select()
     .single()
-  return { data: toCamelCase(data), ...resp }
+    .throwOnError();
+  return toCamelCase(data);
 }
 
 export async function updateParticipant(
@@ -26,45 +27,48 @@ export async function updateParticipant(
   id: number,
   dto: UpdateParticipant,
 ) {
-  const { data, ...resp } = await supabase
-    .from('participants')
+  const { data } = await supabase
+    .from("participants")
     .update(toSnakeCase(dto))
-    .eq('id', id)
+    .eq("id", id)
     .select()
     .single()
-  return { data: toCamelCase(data), ...resp }
+    .throwOnError();
+  return toCamelCase(data);
 }
 
 export async function createSubmission(
   supabase: SupabaseClient,
   dto: InsertSubmission,
 ) {
-  const { data, ...resp } = await supabase
-    .from('submissions')
+  const { data } = await supabase
+    .from("submissions")
     .insert(toSnakeCase(dto))
     .select()
     .single()
-
-  return { data: toCamelCase(data), ...resp }
+    .throwOnError();
+  return toCamelCase(data);
 }
 
 export async function createMultipleSubmissions(
   supabase: SupabaseClient,
   dto: InsertSubmission[],
 ) {
-  const { data, ...resp } = await supabase
-    .from('submissions')
+  const { data } = await supabase
+    .from("submissions")
     .insert(dto.map(toSnakeCase))
     .select()
-  return { data: toCamelCase(data) ?? [], ...resp }
+    .throwOnError();
+  return toCamelCase(data);
 }
 
 export async function insertLog(supabase: SupabaseClient, data: InsertLog) {
-  const { data: log, ...resp } = await supabase
-    .from('demologs')
+  const { data: log } = await supabase
+    .from("demologs")
     .insert(toSnakeCase(data))
     .select()
-  return { data: toCamelCase(log), ...resp }
+    .throwOnError();
+  return toCamelCase(log);
 }
 
 export async function updateSubmissionByKey(
@@ -72,13 +76,14 @@ export async function updateSubmissionByKey(
   key: string,
   dto: UpdateSubmission,
 ) {
-  const { data, ...resp } = await supabase
-    .from('submissions')
+  const { data } = await supabase
+    .from("submissions")
     .update(toSnakeCase(dto))
-    .eq('key', key)
+    .eq("key", key)
     .select()
     .single()
-  return { data: toCamelCase(data), ...resp }
+    .throwOnError();
+  return toCamelCase(data);
 }
 
 export async function updateSubmissionById(
@@ -86,25 +91,26 @@ export async function updateSubmissionById(
   id: number,
   dto: UpdateSubmission,
 ) {
-  const { data, ...resp } = await supabase
-    .from('submissions')
+  const { data } = await supabase
+    .from("submissions")
     .update(toSnakeCase(dto))
-    .eq('id', id)
+    .eq("id", id)
     .select()
     .single()
-  return { data: toCamelCase(data), ...resp }
+    .throwOnError();
+  return toCamelCase(data);
 }
 
 export async function addSubmissionError(
   supabase: SupabaseClient,
   dto: InsertSubmissionError,
 ) {
-  await supabase.from('submission_errors').insert(toSnakeCase(dto))
+  await supabase.from("submission_errors").insert(toSnakeCase(dto));
 }
 
 export async function addMultipleSubmissionErrors(
   supabase: SupabaseClient,
   dtos: InsertSubmissionError[],
 ) {
-  await supabase.from('submission_errors').insert(dtos.map(toSnakeCase))
+  await supabase.from("submission_errors").insert(dtos.map(toSnakeCase));
 }
