f3ae16775b4822350282b984a720f6819b7d1bf3
Villiam Strandh <villiamstrandh@Villiams-MacBook-Pro.local>
2025-02-06 10:49:04 +0100

    eslint config updates

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,11 +1,12 @@
 import js from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import tseslint from "typescript-eslint";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginReact from "eslint-plugin-react";
-import globals from "globals";
 import pluginNext from "@next/eslint-plugin-next";
+import eslintConfigPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 import { config as baseConfig } from "./base.js";
+import reactCompiler from "eslint-plugin-react-compiler";
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
@@ -17,6 +18,7 @@ export const nextJsConfig = [
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
+  reactCompiler.configs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
     languageOptions: {
