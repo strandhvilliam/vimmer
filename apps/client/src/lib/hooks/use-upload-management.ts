529e201305433432d461d1284cf28764669750a2
Villiam Strandh <villiamstrandh@Villiams-MacBook-Pro.local>
2025-02-10 23:08:18 +0100

    add upload management

---
+++ apps/client/src/lib/hooks/use-upload-management.ts
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { PresignedObject } from "../actions/initialize-submission";
+import { PhotoWithPresignedUrl, SelectedPhoto } from "../types";
+import { toast } from "@vimmer/ui/hooks/use-toast";
+
+export const useUploadManagement = ({
+  photos,
+  presignedObjects,
+}: {
+  photos: SelectedPhoto[];
+  presignedObjects: PresignedObject[];
+}) => {
+  const [isUploading, setIsUploading] = useState(false);
+
+  const combinedPhotos = photos.reduce((acc, photo) => {
+    const matchingPresigned = presignedObjects.find(
+      (obj) => obj.topicId === photo.topicId,
+    );
+
+    if (!matchingPresigned || !matchingPresigned.submissionId) {
+      console.error("Missing presigned object for photo", photo);
+      return acc;
+    }
+    acc.push({
+      ...photo,
+      presignedUrl: matchingPresigned?.presignedUrl,
+      key: matchingPresigned?.key,
+      submissionId: matchingPresigned?.submissionId,
+    });
+    return acc;
+  }, [] as PhotoWithPresignedUrl[]);
+
+  const handleUpload = async () => {
+    try {
+      setIsUploading(true);
+      await Promise.all(
+        combinedPhotos.map(async (photo) => {
+          if (!photo.presignedUrl) return;
+
+          const response = await fetch(photo.presignedUrl, {
+            method: "PUT",
+            body: photo.file,
+            headers: {
+              "Content-Type": photo.file.type,
+            },
+          });
+          if (!response.ok) {
+            throw new Error("Failed to upload file");
+          }
+        }),
+      );
+    } catch (error) {
+      setIsUploading(false);
+      console.error(error);
+      toast({
+        title: "Upload failed",
+        description: "Please try again later",
+        variant: "destructive",
+      });
+    }
+  };
+
+  return {
+    isUploading,
+    setIsUploading,
+    handleUpload,
+    combinedPhotos,
+  };
+};
