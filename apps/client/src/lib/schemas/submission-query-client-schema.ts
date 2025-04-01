61cfb0c5667c2df22b7b65e522e149cea52b6c33
Villiam Strandh <villiam.strandh@outlook.com>
2025-04-01 12:24:31 +0200

    add server query schema

---
+++ apps/client/src/lib/schemas/submission-query-client-schema.ts
@@ -0,0 +1,26 @@
+import { createSerializer, parseAsInteger, parseAsString } from "nuqs";
+
+export const submissionQueryClientParams = {
+  competitionClassId: parseAsInteger,
+  deviceGroupId: parseAsInteger,
+  participantId: parseAsInteger,
+  participantRef: parseAsString,
+  participantEmail: parseAsString,
+  participantFirstName: parseAsString,
+  participantLastName: parseAsString,
+};
+
+export const submissionQueryClientParamSerializer = createSerializer(
+  submissionQueryClientParams,
+  {
+    urlKeys: {
+      competitionClassId: "cc",
+      deviceGroupId: "dg",
+      participantId: "pid",
+      participantRef: "pr",
+      participantEmail: "pe",
+      participantFirstName: "pf",
+      participantLastName: "pl",
+    },
+  }
+);
---
+++ apps/client/src/lib/schemas/submission-query-server-schema.ts
@@ -0,0 +1,26 @@
+import { createLoader, parseAsInteger, parseAsString } from "nuqs/server";
+
+export const submissionQueryServerParams = {
+  competitionClassId: parseAsInteger,
+  deviceGroupId: parseAsInteger,
+  participantId: parseAsInteger,
+  participantRef: parseAsString,
+  participantEmail: parseAsString,
+  participantFirstName: parseAsString,
+  participantLastName: parseAsString,
+};
+
+export const loadSubmissionQueryServerParams = createLoader(
+  submissionQueryServerParams,
+  {
+    urlKeys: {
+      competitionClassId: "cc",
+      deviceGroupId: "dg",
+      participantId: "pid",
+      participantRef: "pr",
+      participantEmail: "pe",
+      participantFirstName: "pf",
+      participantLastName: "pl",
+    },
+  }
+);
