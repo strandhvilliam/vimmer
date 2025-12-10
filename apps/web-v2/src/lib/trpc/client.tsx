ca5c0182279a091d9885d80daa71726207666fde
Villiam Strandh <villiam.strandh@outlook.com>
2025-12-10 19:54:00 +0100

    wip

---
+++ apps/web-v2/src/lib/trpc/client.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import type { QueryClient } from "@tanstack/react-query"
+import { useState } from "react"
+import { QueryClientProvider } from "@tanstack/react-query"
+import { createTRPCClient, httpBatchStreamLink, loggerLink } from "@trpc/client"
+import { createTRPCContext } from "@trpc/tanstack-react-query"
+
+import type { AppRouter } from "@blikka/api-v2/trpc/routers/_app"
+
+import { createQueryClient } from "./query-client"
+import { authClient } from "../auth/client"
+
+let clientQueryClientSingleton: QueryClient | undefined = undefined
+const getQueryClient = () => {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return createQueryClient()
+  } else {
+    // Browser: use singleton pattern to keep the same query client
+    return (clientQueryClientSingleton ??= createQueryClient())
+  }
+}
+
+export const { useTRPC, TRPCProvider } = createTRPCContext<AppRouter>()
+
+export function TRPCReactProvider(props: { children: React.ReactNode }) {
+  const queryClient = getQueryClient()
+
+  const [trpcClient] = useState(() =>
+    createTRPCClient<AppRouter>({
+      links: [
+        loggerLink({
+          enabled: (op) =>
+            process.env.NODE_ENV === "development" ||
+            (op.direction === "down" && op.result instanceof Error),
+        }),
+        httpBatchStreamLink({
+          url: getBaseUrl() + "trpc",
+          async headers() {
+            const headers = new Headers()
+            headers.set("x-trpc-source", "blikka-web")
+            const { data, error } = await authClient.token()
+            if (error) {
+              console.error(error)
+              return headers
+            }
+            if (data?.token) {
+              headers.set("Authorization", `Bearer ${data.token}`)
+            }
+            return headers
+          },
+        }),
+      ],
+    })
+  )
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TRPCProvider trpcClient={trpcClient} queryClient={queryClient}>
+        {props.children}
+      </TRPCProvider>
+    </QueryClientProvider>
+  )
+}
+
+const getBaseUrl = () => {
+  return (
+    process.env.NEXT_PUBLIC_TRPC_API_URL ||
+    "https://ahjtvn7n4ujnjwzptczktatuh40aefbq.lambda-url.eu-north-1.on.aws/"
+  )
+}
